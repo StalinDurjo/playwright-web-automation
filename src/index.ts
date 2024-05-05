346ea1ad269a0e81a027629fdcdce6f06d35bc71
Stalin <stalindurjo@gmail.com>
2024-05-06 00:17:05 +0600

    index.ts updated

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,1 @@
 // Silence is golden
-
-import MockData from "support/mock-utility/mock-data";
-import MockEnvironment from "support/mock-utility/mock-environment";
-
-(async () => {
-  // const mock = new MockData({ baseUrl: "http://localhost:9999/" });
-  // mock.useMode("API");
-  // mock.setBasicAuth("admin", "password");
-  // mock.createProducts({ limit: 3 });
-
-  const env = new MockEnvironment({ baseUrl: "http://localhost:9999/" });
-  env.useMode("API");
-  env.setBasicAuth("admin", "password");
-  env.enableTax(false);
-})();
